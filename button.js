a73054d75baf4453f89ac122330d83668fd546ff
NidhishR7 <103636261+NidhishR7@users.noreply.github.com>
2022-04-19 14:15:51 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,15 @@
+//change name of button in html and change function name always 
+let hellobtn = document.querySelector('button');
+hellobtn.addEventListener('click', inputMsg);
+
+function inputMsg(){
+ alert('Successfully downloaded pdf');
+  
+}
+let hello = document.querySelector('button1');
+hello.addEventListener('click', Msg);
+
+function Msg(){
+ alert('Successfully downloaded pdf');
+  
+}
